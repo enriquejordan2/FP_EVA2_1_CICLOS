a7b1d7d6c64f8751574a807819a26e424bdf56d3
enriquejordan2 <L24550342@chihuahua2.tecnm.mx>
2024-10-11 20:40:48 -0600

    Add files via upload

---
+++ eva2_1_ciclos/EVA2_1_CICLOS.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_1_ciclos;
+
+/**
+ *
+ * @author Jose Enrique
+ */
+public class EVA2_1_CICLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //EVALUACION DE INSTRUCCIONES:
+        //Ciclos o Bucles
+        //CICLO FOR
+   
+ 
+        
+        for(int i = 1; i <= 10; i++){ // la variable i solo existe dentro del for
+                   // ++ --> operador para sumar 1 unidad
+                   // --> -- resta unidad
+                   //--> *= suma N Valor
+                   // --> -=resta N valor
+                   //--> *= Multiplica por N
+                   // --> /= divide por N
+                   System.out.print(i +"-"); 
+                   
+        }System.out.println("");
+        for(int i = 10; i >= 1; i--){
+                    System.out.print(i +"-");    
+                   }
+        } 
+    }
+    
+
